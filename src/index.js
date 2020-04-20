1afc9a60f7e3713702f95239821d34bae97e875c
danhakin <mdvillalobos@gmail.com>
2020-04-20 13:13:47 +0200

    Bounce off the walls
    Add simple collision detection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
+let ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
@@ -8,7 +9,7 @@ let dy = -2;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095dd";
   ctx.fill();
   ctx.closePath();
@@ -17,6 +18,20 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  updatePosition();
+}
+
+function updatePosition() {
+  // Check top and bottom boundaries
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  // Check right and left boundaries
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
   x += dx;
   y += dy;
 }
